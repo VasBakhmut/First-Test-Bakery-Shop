a1704f21e00122e7b231fb292e682a7f43ee1206
Vas Bakhmut <mizaracnc@gmail.com>
2023-08-13 18:33:38 +1000

    modal-slider-finished

--- js/slider.js
+++ js/slider.js
@@ -2,14 +2,23 @@ const swiper = new Swiper('.swiper-container', {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 20,
-  // breakpoints: {
-  //   768: {
-  //     slidesPerView: 2,
-  //   },
-  //   1280: {
-  //     slidesPerView: 3,
-  //   },
-  // },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1048: {
+      slidesPerView: 3,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+    1920: {
+      slidesPerView: 3,
+    },
+  },
   pagination: {
     el: '.pagination',
     bulletClass: 'pagination__button',
